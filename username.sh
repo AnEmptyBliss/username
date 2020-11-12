58f6adced8116103caf701c620855a19bc49f42f
Diego Lopez <lopezramos@chapman.edu>
2020-11-11 18:32:46 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Diego Lopez
 echo "Enter a username with only lower case letters, digits, and an underscore: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9\_]{3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9\_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a username with only lower case letters, digits, and an underscore: "
